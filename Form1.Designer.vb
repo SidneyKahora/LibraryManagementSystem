213820e1b8ea9c6922434c72119eaa9d51dcb244
ADMIN <ADMIN@DESKTOP-FA7RF2S>
2024-07-11 00:04:38 +0300

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim Label1 As Label
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        txtTitle = New TextBox()
+        txtAuthor = New TextBox()
+        txtYearPublished = New TextBox()
+        txtGenre = New TextBox()
+        btnAdd = New Button()
+        btnUpdate = New Button()
+        btnDelete = New Button()
+        btnClear = New Button()
+        DataGridView1 = New DataGridView()
+        Label1 = New Label()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(111, 50)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(29, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Title"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(111, 103)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(44, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Author"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(111, 161)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(84, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "Year Published"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(111, 219)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(38, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "Genre"
+        ' 
+        ' txtTitle
+        ' 
+        txtTitle.Location = New Point(221, 47)
+        txtTitle.Name = "txtTitle"
+        txtTitle.Size = New Size(100, 23)
+        txtTitle.TabIndex = 4
+        ' 
+        ' txtAuthor
+        ' 
+        txtAuthor.Location = New Point(221, 95)
+        txtAuthor.Name = "txtAuthor"
+        txtAuthor.Size = New Size(100, 23)
+        txtAuthor.TabIndex = 5
+        ' 
+        ' txtYearPublished
+        ' 
+        txtYearPublished.Location = New Point(221, 153)
+        txtYearPublished.Name = "txtYearPublished"
+        txtYearPublished.Size = New Size(100, 23)
+        txtYearPublished.TabIndex = 6
+        ' 
+        ' txtGenre
+        ' 
+        txtGenre.Location = New Point(221, 211)
+        txtGenre.Name = "txtGenre"
+        txtGenre.Size = New Size(100, 23)
+        txtGenre.TabIndex = 7
+        ' 
+        ' btnAdd
+        ' 
+        btnAdd.Location = New Point(76, 307)
+        btnAdd.Name = "btnAdd"
+        btnAdd.Size = New Size(61, 25)
+        btnAdd.TabIndex = 8
+        btnAdd.Text = "Add"
+        btnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' btnUpdate
+        ' 
+        btnUpdate.Location = New Point(187, 307)
+        btnUpdate.Name = "btnUpdate"
+        btnUpdate.Size = New Size(75, 23)
+        btnUpdate.TabIndex = 11
+        btnUpdate.Text = "Update"
+        btnUpdate.UseVisualStyleBackColor = True
+        ' 
+        ' btnDelete
+        ' 
+        btnDelete.Location = New Point(299, 309)
+        btnDelete.Name = "btnDelete"
+        btnDelete.Size = New Size(75, 23)
+        btnDelete.TabIndex = 12
+        btnDelete.Text = "Delete"
+        btnDelete.UseVisualStyleBackColor = True
+        ' 
+        ' btnClear
+        ' 
+        btnClear.Location = New Point(410, 309)
+        btnClear.Name = "btnClear"
+        btnClear.Size = New Size(77, 22)
+        btnClear.TabIndex = 13
+        btnClear.Text = "Clear"
+        btnClear.UseVisualStyleBackColor = True
+        ' 
+        ' DataGridView1
+        ' 
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Location = New Point(436, 65)
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect
+        DataGridView1.Size = New Size(240, 150)
+        DataGridView1.TabIndex = 14
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(DataGridView1)
+        Controls.Add(btnClear)
+        Controls.Add(btnDelete)
+        Controls.Add(btnUpdate)
+        Controls.Add(btnAdd)
+        Controls.Add(txtGenre)
+        Controls.Add(txtYearPublished)
+        Controls.Add(txtAuthor)
+        Controls.Add(txtTitle)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtTitle As TextBox
+    Friend WithEvents txtAuthor As TextBox
+    Friend WithEvents txtYearPublished As TextBox
+    Friend WithEvents txtGenre As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents btnDelete As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents DataGridView1 As DataGridView
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,112 @@
+﻿Imports System.Data.SqlClient
+Imports System.Data
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+Public Class Form1
+
+    Dim con As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim i As Integer
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        con.ConnectionString = "Data Source=DESKTOP-FA7RF2S;Initial Catalog=LibraryDB;Integrated Security=True;Encrypt=True;TrustServerCertificate=True"
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+        disp_data()
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "insert into Book values('" + txtTitle.Text + "','" + txtAuthor.Text + "','" + txtYearPublished.Text + "','" + txtGenre.Text + "')"
+        cmd.ExecuteNonQuery()
+        disp_data()
+        MsgBox("Record Inserted Succesfully")
+
+
+    End Sub
+    Public Sub disp_data()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "select * from Book"
+        cmd.ExecuteNonQuery()
+        Dim dt As New DataTable()
+        Dim da As New SqlDataAdapter(cmd)
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+    End Sub
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        Try
+
+            If con.State = ConnectionState.Open Then
+                con.Close()
+
+            End If
+            con.Open()
+            i = Convert.ToInt32(DataGridView1.SelectedCells.Item(0).Value.ToString())
+
+            cmd = con.CreateCommand()
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "select * from Book where ID =" & i & ""
+            cmd.ExecuteNonQuery()
+            Dim dt As New DataTable()
+            Dim da As New SqlDataAdapter(cmd)
+            da.Fill(dt)
+            Dim dr As SqlClient.SqlDataReader
+            dr = cmd.ExecuteReader(CommandBehavior.CloseConnection)
+            While dr.Read
+                txtTitle.Text = dr.GetString(1).ToString()
+                txtAuthor.Text = dr.GetString(2).ToString()
+                txtYearPublished.Text = dr.GetInt32(3).ToString()
+                txtGenre.Text = dr.GetString(4).ToString()
+            End While
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "update Book set Title = '" + txtTitle.Text + "',Author = '" + txtAuthor.Text + "', YearPublished = '" + txtYearPublished.Text + "',Genre = '" + txtGenre.Text + "'where id =" & i & ""
+        cmd.ExecuteNonQuery()
+        disp_data()
+    End Sub
+
+    Private Sub btnDelete_Click(sender As Object, e As EventArgs) Handles btnDelete.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from Book where Title ='" + txtTitle.Text + " '"
+        cmd.ExecuteNonQuery()
+        disp_data()
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        If con.State = ConnectionState.Open Then
+            con.Close()
+
+        End If
+        con.Open()
+        cmd = con.CreateCommand()
+        cmd.CommandType = CommandType.Text
+        cmd.CommandText = "delete from Book "
+        cmd.ExecuteNonQuery()
+        disp_data()
+    End Sub
+End Class
